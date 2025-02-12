94f252e33d5b9cd15c9f01caa610cb4b25a37043
langadev <alfredolanga360@gmail.com>
2025-02-12 15:49:36 -0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { AnalizeProgresso } from "./components/AnalizeProgresso"
 import Insumos from "./components/Insumos"
+import ProgressTable from "./components/login/ProgressTable"
 export default function App() {
   
   return (
     <div className="bg-zinc-900 w-full h-screen flex justify-center items-center flex-col">
-     <AnalizeProgresso/>
+     <ProgressTable/>
     </div>
    
   )
